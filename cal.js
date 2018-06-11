0e0a334b6ce22313dd66112223f4e99043699c8a
poppop01 <poppeepo2@gmail.com>
2018-06-11 11:36:57 +0700

    bbb

---
+++ cal.js
@@ -0,0 +1,12 @@
+// function add(num1,num2){
+//     return num1 + num2;
+// }
+
+// var add = function(num1,num2){
+//     return num1 + num2;
+// };
+
+
+exports.minus=(num1,num2)=>{
+    return num1 - num2;
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+var cal =require('./cal')
+var cal2 =require('./cal2')
+
+console.log(cal.minus(1,1));
+console.log(cal2.minus(1,1));
